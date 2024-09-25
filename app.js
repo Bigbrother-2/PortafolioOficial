68ac9a1bdd430ecca1559263433fe962ab5f7840
Flitz08 <marcosscotaro1315@gmail.com>
2024-09-24 22:25:07 -0300

    Por seccion del footer

--- app.js
+++ app.js
@@ -27,5 +27,39 @@ barraNavegacion.forEach(function(item) {
 // Seccion de juegos. 
 
 
+const botonPiedra  = document.getElementById("botonPiedra")
+const botonPapel = document.getElementById("botonPapel")
+const botonTijeras = document.getElementById("botonTijeras")
+
+const piedra = "piedra"
+const papel = "papel"
+const tijeras = "tijeras"
+
+botonPiedra.addEventListener("click", function(){
+    play(piedra)
+});
+
+botonPapel.addEventListener("click", function(){
+    play(papel)
+});
+
+botonTijeras.addEventListener("click",function(){
+    play(tijeras)
+});
 
 
+function play(eleccionUsuario){
+    
+
+
+}
+
+
+function eleccionMaquina (){
+    const numeroRandom = Math.floor(Math.random()*3)
+    switch(numeroRandom){
+        case 0: return "Piedra"
+        case 1: return "Papel" 
+        case 2: return "Tijera"
+    }
+}
